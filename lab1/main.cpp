7784719cbd88cf7b3ebd510d8c57cd30b78ce495
Dim_Cham <dchaminov@ya.ru>
2026-02-11 20:53:03 +0300

    Добавил папочку для картинок, которую cmake автоматически копирует рядом с каждым бинарником

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,7 +1,9 @@
 #include <opencv2/opencv.hpp>
 
 #include <cstdlib>
-
+#include <fstream>
 int main() {
+    std::ofstream outfile("resources/check.png");
+    outfile << "P3\n" << 255 << '\n';
     return EXIT_SUCCESS;
 }
