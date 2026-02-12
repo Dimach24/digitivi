09ce97f9450df286f332a21f0fc630ffc0ec7a7a
Dim_Cham <dchaminov@ya.ru>
2026-02-12 09:52:08 +0300

    рефакторинг

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,8 +1,58 @@
 #include <cmath>
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
+cv::Mat getHist(const cv::Mat &image);
+cv::Mat downsample(const cv::Mat &in, size_t q);
+cv::Mat quantise(const cv::Mat &in, size_t levelsNum);
+
+namespace Math {
+double rms(const cv::Mat &in);
+double mean(const cv::Mat &in);
+double std(const cv::Mat &in);
+double std(const cv::Mat &a, const cv::Mat &b);
+} // namespace Math
+
+int main() {
+    std::cout << "Starting application with OpenCV ver: " << CV_VERSION << "\n";
+
+    const cv::Mat image = cv::imread(R"(resources/mordor.png)", 0);
+    if (image.empty())
+        return -1;
+
+    const auto hist = getHist(image);
+    auto downsampled = downsample(image, 4);
+    auto quantised = quantise(image, 4);
+    auto histQuantised = getHist(quantised);
+
+    cv::imshow("Original", image);
+    cv::imshow("Downsampled", downsampled);
+    cv::imshow("HistogramOriginal", hist);
+    cv::imshow("Quantised", quantised);
+    cv::imshow("HistogramQuantised", histQuantised);
+
+    std::cout << "Original RMS:\n\t" << Math::rms(image) << std::endl;
+    for (size_t steps = 2; steps <= 64; steps <<= 1) {
+        auto tmp = quantise(image, steps);
+        std::cout << "========== " << steps << " ==========" << std::endl;
+        std::cout << "Theoretical: " << static_cast<double>(UINT8_MAX / (steps - 1)) / sqrt(12.)
+                  << std::endl
+                  << "Actual:      " << Math::std(image, tmp) << std::endl;
+        auto tmpHist = getHist(tmp);
+        cv::imwrite("quantised_in_" + std::to_string(steps) + "_steps.png", tmp);
+        cv::imwrite("quantised_in_" + std::to_string(steps) + "_steps_hist.png", tmpHist);
+    }
+
+    for (auto &dsq: {4, 8}) {
+        downsampled = downsample(image, dsq);
+        cv::imwrite("downsampled_" + std::to_string(dsq) + "_times.png", downsampled);
+    }
+    cv::imwrite("orig.png", image);
+    cv::imwrite("origHist.png", hist);
+    cv::waitKey(0);
+    return 0;
+}
 
 cv::Mat getHist(const cv::Mat &image) {
     cv::Mat hist = cv::Mat::zeros(1, 256, CV_64FC1);
@@ -101,43 +151,4 @@ double std(const cv::Mat &a, const cv::Mat &b) {
     }
     return sqrt(sum / a.cols / a.rows);
 }
-}
-
-int main() {
-    std::cout << "Starting application with OpenCV ver: " << CV_VERSION << "\n";
-
-    const cv::Mat image = cv::imread(R"(resources/mordor.png)", 0);
-    if (image.empty())
-        return -1;
-
-    const auto hist = getHist(image);
-    auto downsampled = downsample(image, 4);
-    auto quantised = quantise(image, 4);
-    auto histQuantised = getHist(quantised);
-
-    cv::imshow("Original", image);
-    cv::imshow("Downsampled", downsampled);
-    cv::imshow("HistogramOriginal", hist);
-    cv::imshow("Quantised", quantised);
-    cv::imshow("HistogramQuantised", histQuantised);
-
-    std::cout << "Original RMS:\n\t" << Math::rms(image) << std::endl;
-    for (size_t steps = 2; steps <= 64; steps <<= 1) {
-        auto tmp = quantise(image, steps);
-        std::cout << "========== " << steps << " ==========" << std::endl;
-        std::cout << "Theoretical: " << static_cast<double>(UINT8_MAX / (steps - 1)) / sqrt(12.) <<
-                std::endl << "Actual:      " << Math::std(image, tmp) << std::endl;
-        auto tmpHist = getHist(tmp);
-        cv::imwrite("quantised_in_" + std::to_string(steps) + "_steps.png", tmp);
-        cv::imwrite("quantised_in_" + std::to_string(steps) + "_steps_hist.png", tmpHist);
-    }
-
-    for (auto &dsq: {4, 8}) {
-        downsampled = downsample(image, dsq);
-        cv::imwrite("downsampled_" + std::to_string(dsq) + "_times.png", downsampled);
-    }
-    cv::imwrite("orig.png", image);
-    cv::imwrite("origHist.png", hist);
-    cv::waitKey(0);
-    return 0;
-}
+} // namespace Math
