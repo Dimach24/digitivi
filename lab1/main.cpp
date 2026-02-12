98f3bb05f971c74079b844d18baf1b8269df4020
Dim_Cham <dchaminov@ya.ru>
2026-02-12 09:06:29 +0300

    lab1 - finish

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,10 +1,7 @@
-// LAB1.cpp : Defines the entry point for the console application.
-//
-
+#include <cmath>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 
 
 cv::Mat getHist(const cv::Mat &image) {
@@ -46,35 +43,97 @@ cv::Mat downsample(const cv::Mat &in, size_t q) {
     return out;
 }
 
-cv::Mat quantise(const cv::Mat &in, size_t q) {
-    size_t inWidth = in.cols, inHeight = in.rows;
-
-    size_t qStep = UINT8_MAX / q;
+cv::Mat quantise(const cv::Mat &in, size_t levelsNum) {
+    const size_t inWidth = in.cols;
+    const size_t inHeight = in.rows;
+    const size_t qStep = UINT8_MAX / (levelsNum - 1);
 
     cv::Mat out = cv::Mat::zeros(inHeight, inWidth, CV_8U);
+
     for (size_t i = 0; i < inHeight; ++i) {
         for (size_t j = 0; j < inWidth; ++j) {
-            out.at<uchar>(i, j) = in.at<uchar>(i, j) / qStep * qStep;
+            const uint16_t shiftedL = in.at<uchar>(i, j) + qStep / 2;
+            out.at<uchar>(i, j) = shiftedL / qStep * qStep;
         }
     }
+
     return out;
 }
 
+namespace Math {
+double rms(const cv::Mat &in) {
+    double sum = 0;
+    for (int i = 0; i < in.rows; i++) {
+        for (int j = 0; j < in.cols; j++) {
+            sum += pow(in.at<uchar>(i, j), 2);
+        }
+    }
+    return sqrt(sum / in.cols / in.rows);
+}
+
+double mean(const cv::Mat &in) {
+    double sum = 0;
+    for (int i = 0; i < in.rows; i++) {
+        for (int j = 0; j < in.cols; j++) {
+            sum += in.at<uchar>(i, j);
+        }
+    }
+    return sum / in.cols / in.rows;
+}
+
+double std(const cv::Mat &in) {
+    double mean_ = mean(in);
+    double sum = 0;
+    for (int i = 0; i < in.rows; i++) {
+        for (int j = 0; j < in.cols; j++) {
+            sum += pow(in.at<uchar>(i, j) - mean_, 2);
+        }
+    }
+    return sqrt(sum / in.cols / in.rows);
+}
+
+double std(const cv::Mat &a, const cv::Mat &b) {
+    double sum = 0;
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            sum += pow(a.at<uchar>(i, j) - b.at<uchar>(i, j), 2);
+        }
+    }
+    return sqrt(sum / a.cols / a.rows);
+}
+}
+
 int main() {
-    cv::Mat image = cv::imread(R"(resources/mordor.png)", 0);
+    std::cout << "Starting application with OpenCV ver: " << CV_VERSION << "\n";
+
+    const cv::Mat image = cv::imread(R"(resources/mordor.png)", 0);
     if (image.empty())
         return -1;
 
+    const auto hist = getHist(image);
+    const auto downsampled = downsample(image, 4);
+    const auto quantised = quantise(image, 4);
+    const auto histQuantised = getHist(quantised);
 
-    // image = quantise(image, 10);
-    cv::Mat hist = getHist(image);
-    imshow("Picture", image);
-    imshow("Histogram", hist);
+    cv::imshow("Original", image);
+    cv::imshow("Downsampled", downsampled);
+    cv::imshow("HistogramOriginal", hist);
+    cv::imshow("Quantised", quantised);
+    cv::imshow("HistogramQuantised", histQuantised);
 
-    std::cout << "Hello World\n";
-    std::cout << "OpenCV ver: " << CV_VERSION << "\n";
+    std::cout << "Original RMS:\n\t" << Math::rms(image) << std::endl;
+    for (size_t steps = 2; steps <= 64; steps *= 2) {
+        auto tmp = quantise(image, steps);
+        std::cout << "========== " << steps << " ==========" << std::endl;
+        std::cout << "Theoretical: " << static_cast<double>(UINT8_MAX / (steps - 1)) / sqrt(12.) <<
+                std::endl << "Actual:      " << Math::std(image, tmp) << std::endl;
+    }
+
+    size_t i = 0;
+    for (auto &img: {image, hist, downsampled, quantised, histQuantised}) {
+        cv::imwrite(std::to_string(i++) + ".png", img);
+    }
     cv::waitKey(0);
-    cv::moveWindow("Picture", 0, 0);
-    cv::moveWindow("Histogram", image.cols, 0);
+
     return 0;
 }
