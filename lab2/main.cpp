8541917ad143317fb7c3c8c60e12e30bd8d6fe99
Dimach24 <dchaminov@ya.ru>
2026-02-18 18:32:35 +0300

    заготовка на вторую лабу

---
+++ lab2/main.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+int main() {
+    std::cout << "Starting application with OpenCV ver: " << CV_VERSION << "\n";
+
+    return 0;
+}
